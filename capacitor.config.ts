3aad942d3cd9b11ccc1e8c22c063ebf3fceca547
USRVI-LC4 <USRVI-LC4@duoc.acad>
2024-08-20 14:09:24 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyecto005_movil',
+  webDir: 'www'
+};
+
+export default config;
